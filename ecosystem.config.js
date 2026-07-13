76bf7c5d182e45cbb53802d035551786f8f4f090
INTIGRIN <askarovrauan10@gmail.com>
2026-07-14 02:22:56 +0500

    fix: update ecosystem.config.js to run next.js in fork mode via node bin directly

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -23,9 +23,10 @@ module.exports = {
     {
       name: 'learnhub-web',
       cwd: './apps/web',
-      script: 'node_modules/.bin/next',
+      script: 'node_modules/next/dist/bin/next',
       args: 'start -p 3000',
       instances: 1,
+      exec_mode: 'fork',
       autorestart: true,
       watch: false,
       max_memory_restart: '512M',
